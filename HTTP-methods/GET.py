e646e48195e228effaa19e35e0eae9970f962ea8
Griive <igrishkin77@icloud.com>
2024-11-10 03:08:41 +0300

    method update

--- HTTP-methods/GET.py
+++ HTTP-methods/GET.py
@@ -1,10 +1,18 @@
 import requests
 
-
 server = ""
 
 body = {
     "email": "igrishkin77@gmail.com",
+    "PostIndex": " ",
+    "Country": " ",
+    "City": " ",
+    "Street": " ",
+    "Building": " ",
+    "House": " ",
+    "Apartment": " ",
+    "AsDefault": " ",
+    "CreatedAt": " ",
 }
 
 r = requests.get(server, json=body)
--- HTTP-methods/POST.py
+++ HTTP-methods/POST.py
@@ -1,10 +1,18 @@
 import requests
 
-
 server = ""
 
 body = {
     "email": "igrishkin77@gmail.com",
+    "postIndex": " ",
+    "country": " ",
+    "city": " ",
+    "street": " ",
+    "building": " ",
+    "house": " ",
+    "apartment": " ",
+    "asDefault": " ",
+    "createdAt": " ",
 }
 
 r = requests.post(server, json=body)
